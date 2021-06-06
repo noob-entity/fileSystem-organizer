73b63bdddee5c2e6691f9c044135db3760c0f95d
maya <mayakataria58@gmail.com>
2021-06-06 15:12:02 +0530

    first commit

--- command/organize.js
+++ command/organize.js
@@ -7,31 +7,88 @@ let types = {
   app: ['exe', 'dmg', 'pkg', "deb"]
 }
 module.exports = types;
-    function organizedir(src){
-    let dir = path.join(src,"organized");
+function isFileOrNot(src) {
+  return fs.lstatSync(src).isFile();
+}
+function readContent(src) {
+  return fs.readdirSync(src);
+}
+function copyFile(src, desFolder) {
 
-    if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir);
-    }
-    organizedir(src,dir);
-    
+  if (fs.existsSync(desFolder) == false) {
+      fs.mkdirSync(desFolder);
+  }
+  
+  let fileName = path.basename(src)
+  // console.log("dest ", path.join(desFolder, fileName))
+  // exist
+  // access
+  // organized folder 
+  fs.copyFileSync(src, path.join(desFolder, fileName));
+}
+function getdestName(src) {
+  let ext = src.split(".").pop();
+  for (let key in types) {
+      for (let i = 0; i < types[key].length; i++) {
+          if (ext == types[key][i]) {
+              return key;
+          }
+      }
+  }
+  return "others";
 
 }
-function organize(src,dest){
-  let isFile=fs.lstatSync(src).isFile();
-  if(isFile){
-    console.log(src);
+function organize(src, dest) {
+  // file ->
+  let isFile = isFileOrNot(src);
+  if (isFile == true) {
+      // copy to organized files folder -> segregate 
+      // identify 
+      // get foldername in which i have to put my file
+      let folderName = getdestName(src);
+      console.log(folderName, " -> ", src);
+      // src/organized_files/media
+      copyFile(src, path.join(dest, folderName));
   }
-  else{
-    console.log(src);
-    let fdirnames= readcontent(src);
-    for(let i=0;i<fdirnames.length;i++){
-      let child=fdirnames[i];
-      let dirNamepath =path.join(src,child);
-      organize(dirNamepath,dest);
-    }
+  else {
+      // folder -> recursion
+      // print
+      // content read from os
+      let fDirnames = readContent(src);
+      // recursion 
+      // console.log(fDirnames);
+      for (let i = 0; i < fDirnames.length; i++) {
+          let child = fDirnames[i];
+          //    good practice??
+          // let dirNamepath = src + "\\" + child;
+          let dirNamepath = path.join(src, child);
+          organize(dirNamepath, dest)
+      }
   }
 }
+    function organizeFiles(src){
+      // create organize file folder in src
+      let destFolderPath = path.join(src, "organized_files");
+      console.log(destFolderPath);
+      if (fs.existsSync(destFolderPath) == false) {
+          fs.mkdirSync(destFolderPath);
+      }
+      // deep copy all the files in organize file folder
+      organize(src, destFolderPath);
+  }
+  // path of the folder which we want to organize
+  // node mycli.js organize "f-path"
+  
+  function organizefn(src) {
+      // create organize files folder 
+      if (src == undefined) {
+          src = process.cwd();
+      }
+      console.log(src);
+      organizeFiles(src);
+
+}
+
 
 module.exports={
   fn:organizefn
--- mycli.js
+++ mycli.js
@@ -1,19 +1,26 @@
-let helperFile=require("./command/help");
-let organizeFile=require("./command/organize");
-let viewFile=require("./command/view");
-let input=process.argv.slice(2);
-let command =input[0];
+let helperFile = require("./command/help");
+let viewFile = require("./command/view");
+let organizeFile = require("./command/organize");
+let input = process.argv.slice(2);
+// mycli view "fname" tree
+// node mycli.js [view ,dirName]
+let command = input[0];
+
+
 switch (command) {
     case "view":
-        viewFile.fn(input[1],input[2]);
+        viewFile.fn(input[1], input[2]);
         break;
     case "organize":
-        organizeFile.fn();
+        organizeFile.fn(input[1]);
         break;
     case "help":
         helperFile.fn();
         break;
     default:
-        console.log("Wrong command type help for all commands");
+        console.log("wrong command type help for all the commands");
         break;
 }
+//view
+//organize
+//help
